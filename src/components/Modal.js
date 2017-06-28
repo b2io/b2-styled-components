fb463c2150f8807cb1f113751e60e96aeb149bfa
Caitlin Steinert <caitlinsteinert@gmail.com>
2017-06-28 11:52:21 -0400

    PR review updates

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,12 +3,11 @@ import { em, rem, z } from '../styles/Functions';
 
 const Modal = styled.div`
   border: 2px solid ${props => props.theme.color.black};
-  --border-radius: var(--border-radius);
   display: block;
   font-size: ${props => rem(props.theme.font.size.base)};
+  left: 50%;
   padding: ${em(16)} ${em(32)};
   text-align: center;
-  left: 50%;
   transform: translateX(-50%);
   ${z('modal', 'absolute')};
 `;
